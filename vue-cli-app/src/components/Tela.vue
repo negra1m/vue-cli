f5761026e1828341dd574b59a545d05e556972f6
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-12 21:56:28 -0300

    Testing V-Text Directive

---
+++ vue-cli-app/src/components/Tela.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h3 v-text="titulo"></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tela",
+  //data contem todas as propriedades que serao manipuladas dentro desse mesmo componente
+  data() {
+    return {
+      titulo: "Title"
+    };
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
