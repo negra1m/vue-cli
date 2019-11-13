94f2b3013ce6ca092638da3b95d07d5cc93ba766
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-12 22:37:39 -0300

    Testing v-FOR on CLI version

--- vue-cli-app/src/components/Tela.vue
+++ vue-cli-app/src/components/Tela.vue
@@ -6,6 +6,11 @@
     <span v-if="seen">Mostra isso?</span>
     <!-- Todos os atributos podem ser linkados com o v-bind, alguns deles possuem modifiers e.g.: disabled -->
     <img v-bind:src="photo.url" v-bind:alt="photo.alt" />
+    <ul>
+      <li v-for="photo in photos" :key="photo.id">
+        <img :src="photo.url" :alt="photo.alt" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,7 +26,21 @@ export default {
         url:
           "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
         alt: "Doguinho"
-      }
+      },
+      photos: [
+        {
+          id: 0,
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          alt: "Doguinho"
+        },
+        {
+          id: 1,
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM",
+          alt: "Gato"
+        }
+      ]
     };
   },
   props: {
