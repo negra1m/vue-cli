e70b6d00be803419695bbb7e5b7e3e459accf026
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-12 22:02:34 -0300

    Fix: strin to string correction

--- vue-cli-app/src/components/Tela.vue
+++ vue-cli-app/src/components/Tela.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{msg}}</h1>
     <h3 v-text="titulo"></h3>
-    <p v-text="titulo + new Date().toLocaleStrin()"></p>
+    <p v-text="titulo + new Date().toLocaleString()"></p>
 
     <span v-if="seen">Mostra isso?</span>
   </div>
